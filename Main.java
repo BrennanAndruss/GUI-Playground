2f217d42c94cddfc44833da84669f3729784d4ce
Andruss <brennana46274@stu.powayusd.com>
2023-04-21 14:31:54 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+/**
+ * Brennan Andruss
+ * GUI Playground
+ * 
+ * Description: Learn how to create a GUI
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String args[]){
+
+        // Creating the frame
+        JFrame frame = new JFrame("Project 2");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+
+        // Creating menu bar
+        JMenuBar menuBar = new JMenuBar();
+        JMenu m1 = new JMenu("File");
+        JMenu m2 = new JMenu("Edit");
+        menuBar.add(m1);
+        menuBar.add(m2);
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+        frame.setVisible(true);
+
+        // Creating menu dropdowns
+        JMenuItem m11 = new JMenuItem("New");
+        JMenuItem m12 = new JMenuItem("Open");
+        JMenuItem m13 = new JMenuItem("Save");
+        m1.add(m11);
+        m1.add(m12);
+        m1.add(m13);
+
+        JMenuItem m21 = new JMenuItem("Undo");
+        JMenuItem m22 = new JMenuItem("Redo");
+        JMenuItem m23 = new JMenuItem("Copy");
+        JMenuItem m24 = new JMenuItem("Paste");
+
+
+        /*
+        // Creating buttons
+        JButton b1 = new JButton("Press 1");
+        JButton b2 = new JButton("Press 2");
+        frame.getContentPane().add(b1);
+        frame.getContentPane().add(b2);
+        frame.setVisible(true);
+        */
+    }
+}
